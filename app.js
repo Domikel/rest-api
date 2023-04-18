6fbf36768b45179165233d8cf93b2c9529c12a59
Miguel Angel John <im.mikel@hotmail.com>
2023-04-18 10:32:49 +0200

    finished

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
 
-
 mongoose.connect(
   "mongodb+srv://immikel:JFzl8BliKy3NrA8P@cluster0.hmbiy9y.mongodb.net/test",
   { useNewUrlParser: true, useUnifiedTopology: true }
@@ -12,10 +11,6 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     userName: {
-      type: String,
-      required: true,
-    },
-    firstName: {
       type: String,
       required: true,
     },
@@ -45,40 +40,35 @@ const userSchema = new Schema(
 
 const User = mongoose.model("User", userSchema);
 
-
-
 const app = express();
 
 app.use(json());
 
-
-
 const port = process.env.PORT || 2000;
 
-
-
-app.get("/users", async (req, res)=>{
- 
+app.get("/users", async (req, res) => {
   const users = await User.find();
 
   res.json(users);
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   const newUser = new User({
     userName: req.body.userName,
-    firstname: req.body.username,
-    lastname:req.body.lastname,
-    email:req.body.email,
-    password:req.body.password
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
   });
 
   const savedUser = await newUser.save();
 
   res.status(201).json(savedUser);
 
-  if(!userName){  return res.status(400).send('username is required')}
-})
+  if (!userName) {
+    return res.status(400).send("username is required");
+  }
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
