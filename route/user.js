baf0cae8d291f85cc414ab69ee1cee36f32046d7
Alex <alexgrubor@gmail.com>
2023-04-18 13:46:46 +0200

    validator

--- route/user.js
+++ route/user.js
@@ -1,18 +1,17 @@
-import {Router} from "express";
+import { Router } from "express";
 import { deleteUser } from "../controllers/deleteUser.js";
 import { getSingleUser } from "../controllers/getUserById.js";
 import createUser from "../controllers/createUser.js";
 import displayAllUsers from "../controllers/displayAllUsers.js";
 import loginUser from "../controllers/loginUser.js";
 import auth from "../middleware/auth.js";
+import { rules } from "../middleware/validator.js";
 const router = Router();
-
 
 router.get("/users", displayAllUsers);
 router.post("/users/login", loginUser);
+router.post("/users", rules, createUser);
 router.get("/users/:id", auth, getSingleUser);
-router.post("/users", createUser);
-router.delete("/users/:id",auth,  deleteUser);
-
+router.delete("/users/:id", auth, deleteUser);
 
 export default router;
