ca2146636af4ecbdd879dbb0b528284a24f2b5b3
Alex <alexgrubor@gmail.com>
2023-04-18 11:32:50 +0200

    refactoring init

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
-
-
-mongoose.connect(
-  "mongodb+srv://immikel:JFzl8BliKy3NrA8P@cluster0.hmbiy9y.mongodb.net/test",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    firstName : {
-        type: String,
-        required: true
-    },
-    lastName : {
-        type: String,
-        required: true
-    },
-    email : {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password : {
-        type: String,
-        required: true
-    }
- 
-}, {
-  timestamps: true,
-  versionKey:false
-});
-
-const User = mongoose.model("User", userSchema);
-
-
-
+import dotenv from "dotenv";
 const app = express();
 
+dotenv.config();
 app.use(json());
-
-
 
 const port = process.env.PORT || 2000;
 
 
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get("/users", async (req, res)=>{
- 
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/users", async (req, res) => {
   const users = await User.find();
 
   res.json(users);
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   const newUser = new User({
-    username: req.body.username
+    username: req.body.username,
   });
 
   const savedUser = await newUser.save();
 
   res.json(savedUser);
-})
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
