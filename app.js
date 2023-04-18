f7a88750d9a0388a1eb8be177231a432227e1d62
Alex <alexgrubor@gmail.com>
2023-04-18 09:35:28 +0200

    fix server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 
 
@@ -19,22 +19,30 @@ const User = mongoose.model("User", userSchema);
 
 const app = express();
 
+app.use(json());
+
+
+
 const port = process.env.PORT || 2000;
 
 
 
-app.get("/users", (req, res)=>{
+app.get("/users", async (req, res)=>{
+ 
+  const users = await User.find();
 
-  User.find()
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send("User not found");
-      }
-      res.send(user);
-    })
-    .catch((err) => console.log(err));
-  
+  res.json(users);
 });
+
+app.post('/users', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username
+  });
+
+  const savedUser = await newUser.save();
+
+  res.json(savedUser);
+})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
