247c67b270b9b4a08eb63affd832cd347fd607ac
Alex <alexgrubor@gmail.com>
2023-04-19 12:18:12 +0200

    refactored comment route

---
+++ controllers/commentsConntroller.js
@@ -0,0 +1,53 @@
+export const addNewComment = async (req, res) => {
+  try {
+    const comment = new Comment(req.body);
+    await comment.save();
+    res.status(200).json({
+      status: "success",
+      data: {
+        comment,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+
+export const deleteComment =  async (req, res) => {
+    try {
+      const comment = await Comment.findByIdAndDelete(req.params.id);
+      if (!comment) {
+        return res.status(404).json({
+          message: "comment not found",
+          
+        })
+      }
+      res.status(200).json({ message: "delete successfully" });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+
+export const seeAllCommentsByPost = async (req, res) => {
+    try {
+        const comments = await Comment.find({ post: req.params.id })
+        .populate("user")
+        .populate("post");
+        console.log(comments.length);
+        res.status(200).json({
+        status: "success",
+        data: {
+            comments,
+        },
+        });
+    } catch (err) {
+        res.status(400).json({
+        status: "fail",
+        message: err,
+        });
+    }
+}
--- route/comments.js
+++ route/comments.js
@@ -1,24 +1,11 @@
 import { Router } from "express";
-import { Comment } from "../models/comments.js";
+import { addNewComment, deleteComment, seeAllCommentsByPost } from "../controllers/commentsConntroller.js";
 
 
 const router = Router();
 
-router.post("/comments", async (req, res) => {
-  const { text, user, post } = req.body;
-  const newComment = await Comment.create({ text, user, post });
-  res.status(201).json({message: "'commnt saved successfully", data:newComment})
-});
+router.post("/comments", addNewComment);
+router.get("/comments/:id", seeAllCommentsByPost);
 
-router.delete("/comments/:id", async (req, res) => {
-  try {
-    const comment = await Comment.findByIdAndDelete(req.params.id);
-    if (!comment) {
-      return res.status(404).jasonsend();
-    }
-    res.status(200).json({ message: "delete successfully" });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.delete("/comments/:id",deleteComment);
 
