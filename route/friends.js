1b7c5fb32712d8aed13164614943457876136431
dcistudent <dci-student@dcistudent>
2023-04-19 10:33:53 +0200

    Added friends and likes code

--- route/friends.js
+++ route/friends.js
@@ -1,5 +1,77 @@
-//add a new friend 
+//add a new friend
+
+app.post("/users/:userId/friends", async (req, res) => {
+  const userId = req.params.userId;
+  const friend = req.body.friend;
+
+  try {
+    const result = await db
+      .collection("users")
+      .updateOne({ _id: ObjectId(userId) }, { $push: { friends: friend } });
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to add friend." });
+  }
+});
 //see all friends
+
+app.get("/users/:userId/friends", async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const result = await db
+      .collection("users")
+      .findOne({ _id: ObjectId(userId) }, { friends: 1 });
+    res.status(200).json(result.friends);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to retrieve friends." });
+  }
+});
 //see a single friend
+
+app.get("/users/:userId/friends/:friendEmail", async (req, res) => {
+  const userId = req.params.userId;
+  const friendEmail = req.params.friendEmail;
+
+  try {
+    const result = await db
+      .collection("users")
+      .findOne(
+        { _id: ObjectId(userId), "friends.email": friendEmail },
+        { "friends.$": 1 }
+      );
+    if (result) {
+      res.status(200).json(result.friends[0]);
+    } else {
+      res.status(404).json({ error: "Friend not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to retrieve friend." });
+  }
+});
 //delete friend
 
+app.delete("/users/:userId/friends/:friendEmail", async (req, res) => {
+  const userId = req.params.userId;
+  const friendEmail = req.params.friendEmail;
+
+  try {
+    const result = await db
+      .collection("users")
+      .updateOne(
+        { _id: ObjectId(userId) },
+        { $pull: { friends: { email: friendEmail } } }
+      );
+    if (result.modifiedCount === 1) {
+      res.status(200).json({ message: "Friend deleted successfully." });
+    } else {
+      res.status(404).json({ error: "Friend not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to delete friend." });
+  }
+});
--- route/likes.js
+++ route/likes.js
@@ -1,3 +1,64 @@
 //add like
+
+app.put("/posts/:postId/like", async (req, res) => {
+  const postId = req.params.postId;
+
+  try {
+    const result = await db
+      .collection("posts")
+      .updateOne({ _id: ObjectId(postId) }, { $inc: { likes: 1 } });
+    if (result.modifiedCount === 1) {
+      res.status(200).json({ message: "Like added successfully." });
+    } else {
+      res.status(404).json({ error: "Post not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to add like." });
+  }
+});
+
 //delete like
+
+app.put("/posts/:postId/unlike", async (req, res) => {
+  const postId = req.params.postId;
+
+  try {
+    const result = await db
+      .collection("posts")
+      .updateOne(
+        { _id: ObjectId(postId), likes: { $gt: 0 } },
+        { $inc: { likes: -1 } }
+      );
+    if (result.modifiedCount === 1) {
+      res.status(200).json({ message: "Like removed successfully." });
+    } else if (result.modifiedCount === 0) {
+      res.status(404).json({ error: "Post not found or has no likes." });
+    } else {
+      res.status(400).json({ error: "Unexpected error occurred." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to remove like." });
+  }
+});
+
 //get all likes
+
+app.get("/posts/:postId/likes", async (req, res) => {
+  const postId = req.params.postId;
+
+  try {
+    const post = await db
+      .collection("posts")
+      .findOne({ _id: ObjectId(postId) }, { likes: 1 });
+    if (post) {
+      res.status(200).json({ likes: post.likes });
+    } else {
+      res.status(404).json({ error: "Post not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Unable to retrieve likes." });
+  }
+});
